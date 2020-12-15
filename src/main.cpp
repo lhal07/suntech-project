2e3e1da9b355241d947b4bfd3400de4c42d839ec
Luis Lourenço <lhal07@gmail.com>
2020-12-14 23:27:49 -0300

    refactor: apply changes for new server

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
 #include <server.h>
-
-static const char *const HEADER = "\nInitial Project\n\n";
-static const char *const USAGE = "Usage.\n";
+#include <config.h>
+#include <boost/asio.hpp>
 
 int main(int argc, const char *argv[]) {
-  std::cout << HEADER;
-
-  // ensure the correct number of parameters are used.
   if (argc < 1) {
-    std::cout << USAGE;
+    std::cout << "No arguments required";
     return 1;
   }
 
-  Server tc = Server("Hi!");
-  tc.sayHi();
+  try {
+    boost::asio::io_context io_context;
+    Config config = Config("file.conf");
+    Server server = Server(io_context, config);
+    server.run();
+    io_context.run();
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
 
   return 0;
 }
