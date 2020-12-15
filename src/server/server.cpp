fb8db7491cbbe2c33aea3e2d45ab73673b9b8812
Luis Lourenço <lhal07@gmail.com>
2020-12-14 23:26:26 -0300

    feat: add server class

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,5 +1,23 @@
 #include "server.h"
 
-void Server::sayHi() {
-    cout << this->salutation;
+void Server::run() {
+  cout << "Running server" << endl;
+  this->listen();
+}
+
+void Server::listen() {
+  cout << "Listening for connections" << endl;
+  Connection::pointer new_connection = Connection::create(this->m_io_context,this->m_config);
+
+  this->m_acceptor.async_accept(new_connection->socket(),
+                        boost::bind(&Server::connect, this, new_connection,
+                        boost::asio::placeholders::error));
+}
+
+void Server::connect(Connection::pointer new_connection, const boost::system::error_code& error) {
+  cout << "New connection received" << endl;
+  if (!error) {
+    new_connection->start();
+  }
+  this->listen();
 }
--- src/server/server.h
+++ src/server/server.h
@@ -2,22 +2,31 @@
 #define CMAKE_SERVER_H
 
 #include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include "../connection/connection.h"
 
+using boost::asio::ip::tcp;
 using namespace std;
 
 class Server {
 public:
-  explicit Server(string salutation) {
-    this->salutation = salutation;
-  }
+  Server(boost::asio::io_context& io_context, Config& config)
+    : m_io_context(io_context),
+    m_config(config),
+    m_acceptor(io_context, tcp::endpoint(tcp::v4(), config.getPort())) {}
 
-  ~Server() {
-  };
+  ~Server() {}
 
-  void sayHi();
+  void run();
 
-protected:
-  string salutation;
+private:
+  void listen();
+  void connect(Connection::pointer, const boost::system::error_code&);
+
+  boost::asio::io_context& m_io_context;
+  Config& m_config;
+  tcp::acceptor m_acceptor;
 };
 
 #endif //CMAKE_SERVER_H
